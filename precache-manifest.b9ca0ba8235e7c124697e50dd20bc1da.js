8f96f1c76bf0d996bd773e69aa1212a8e22fb708
Gamewolf <joakimthegamer@yahoo.se>
2020-03-26 17:34:39 +0100

    Changed things

---
+++ precache-manifest.b9ca0ba8235e7c124697e50dd20bc1da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c953f141ee211e39e5d790d009f25234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0c1cdb9bde3ceb326c7",
+    "url": "/static/js/2.41ee83b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41ee83b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7e40c9472b285dc9e5",
+    "url": "/static/js/main.b70106fe.chunk.js"
+  },
+  {
+    "revision": "cb37079125863d1a7ac1",
+    "url": "/static/js/runtime-main.18b57e9e.js"
+  }
+]);
